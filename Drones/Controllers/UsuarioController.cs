1d69b1af02afc57c9bbe8d9be40420e7d2f3233a
Danel <danelcaballero7@gmail.com>
2022-11-27 04:25:53 -0800

    Para Crear Usuarios y drones tienes q estar autenticado con el usuario "Juan" password "123."

---
+++ Drones/Controllers/UsuarioController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Drones.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Drones.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : Controller
+    {
+        public IConfiguration _configuration;
+        public UsuarioController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("login")]
+        public dynamic IniciarSesion([FromBody]Object optData)
+        {
+            var data = JsonConvert.DeserializeObject<dynamic>(optData.ToString());
+
+            string user = data.usuario.ToString();
+            string password = data.password.ToString();
+
+            Usuario usuario = Usuario.DB().Where(u => u.usuario == user && u.password == password).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return new
+                {
+                    succes = false,
+                    message = "Credenciales incorrectas",
+                    result = ""
+                };
+            }
+            var jwt = _configuration.GetSection("Jwt").Get<Jwt>();
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,jwt.Subject),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat,DateTime.UtcNow.ToString()),
+                new Claim("id", usuario.idUsuario),
+                new Claim("usuario", usuario.usuario)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key));
+            var singIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                jwt.Issuer,
+                jwt.Audience,
+                claims,
+                expires: DateTime.Now.AddMinutes(4),
+                signingCredentials: singIn
+                );
+            return new
+            {
+                succes = true,
+                message = "exito",
+                result = new JwtSecurityTokenHandler().WriteToken(token)
+            };
+        }
+    }
+}
